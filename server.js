e5c13a35a25d46b52d1dd97b8adbefeb24454a71
ankita132 <2ankitasahoo13@gmail.com>
2018-04-09 04:06:57 +0530

    server

--- server.js
+++ server.js
@@ -1,11 +1,5 @@
-// Use the environment variable or use a given port
-const PORT = process.env.PORT || 8080;
-
-// Create a server, uses `handleRequest` which is function that takes
-// care of providing requested data
-const server = http.createServer(handleRequest);
-
-// Start the server
-server.listen(PORT, () => {
-  console.log('Server listening on: http://localhost:%s', PORT);
+var connect = require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic(__dirname)).listen(8080, function(){
+    console.log('Server running on 8080...');
 });
