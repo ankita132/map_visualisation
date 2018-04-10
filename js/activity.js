e3c040e684bd71fd968bd2af3799b90022dcb054
ankita132 <2ankitasahoo13@gmail.com>
2018-04-11 05:08:30 +0530

    added 2015 data

--- js/activity.js
+++ js/activity.js
@@ -11,37 +11,35 @@ $(document).ready(function(){
 	$('#inputyear').on('change', function() {
    		var year = $(this).val();
    		if(year == "2017"){
+   			$('#inputcountry').html('');
+   			$('#inputstadium').html('');
 		$.getJSON('./jsons/2017_participate.geojson', function(data){
-			for(var i = 0; i< data.features.length; i++){   
-			   $('#inputcountry').append('<option value="">' + data.features[i].properties.name + '</option>');
-			}
-			});
-		$.getJSON('./jsons/2017_winner.geojson', function(data){
+			$('#inputcountry').append('<option selected disabled value="">Choose</option>');
 			for(var i = 0; i< data.features.length; i++){   
 			   $('#inputcountry').append('<option value="">' + data.features[i].properties.name + '</option>');
 			}
 			});
 		$.getJSON('./jsons/2017_stadium.geojson', function(data){
+			$('#inputstadium').append('<option selected disabled value="">Choose</option>');
 			for(var i = 0; i< data.features.length; i++){   
-			   $('#inputstadium').append('<option value="">' + data.features[i].properties.name + '</option>');
+			   $('#inputstadium').append('<option value="">' + data.features[i].properties.stadium + '</option>');
 			}
 			});
 	}
 
 	if(year == "2015"){
+			$('#inputcountry').html('');
+   			$('#inputstadium').html('');
 		$.getJSON('./jsons/2015_participate.geojson', function(data){
-			for(var i = 0; i< data.features.length; i++){   
-			   $('#inputcountry').append('<option value="">' + data.features[i].properties.name + '</option>');
-			}
-			});
-		$.getJSON('./jsons/2015_winner.geojson', function(data){
+			$('#inputcountry').append('<option selected disabled value="">Choose</option>');
 			for(var i = 0; i< data.features.length; i++){   
 			   $('#inputcountry').append('<option value="">' + data.features[i].properties.name + '</option>');
 			}
 			});
 		$.getJSON('./jsons/2015_stadium.geojson', function(data){
+			$('#inputstadium').append('<option selected disabled value="">Choose</option>');
 			for(var i = 0; i< data.features.length; i++){   
-			   $('#inputstadium').append('<option value="">' + data.features[i].properties.name + '</option>');
+			   $('#inputstadium').append('<option value="">' + data.features[i].properties.stadium + '</option>');
 			}
 			});
 	}
