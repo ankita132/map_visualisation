b305d1dcafde829a6a00a25d2b8858cbafd4f96a
ankita132 <2ankitasahoo13@gmail.com>
2018-04-11 04:37:10 +0530

    added form

--- js/activity.js
+++ js/activity.js
@@ -1,5 +1,50 @@
 $(document).ready(function(){
-	$('#sidebarCollapse').on('click', function(){
-		$('#sidebar').toggleClass('active');
-	})
+	$('.data').on('click', function(){
+		$('#map').fadeOut('fast');
+		$('.form-data').fadeIn('fast');
+	});
+	$('.year').on('click', function(){
+		$('#map').fadeIn('fast');
+		$('.form-data').fadeOut('fast');
+	});
+		
+	$('#inputyear').on('change', function() {
+   		var year = $(this).val();
+   		if(year == "2017"){
+		$.getJSON('./jsons/2017_participate.geojson', function(data){
+			for(var i = 0; i< data.features.length; i++){   
+			   $('#inputcountry').append('<option value="">' + data.features[i].properties.name + '</option>');
+			}
+			});
+		$.getJSON('./jsons/2017_winner.geojson', function(data){
+			for(var i = 0; i< data.features.length; i++){   
+			   $('#inputcountry').append('<option value="">' + data.features[i].properties.name + '</option>');
+			}
+			});
+		$.getJSON('./jsons/2017_stadium.geojson', function(data){
+			for(var i = 0; i< data.features.length; i++){   
+			   $('#inputstadium').append('<option value="">' + data.features[i].properties.name + '</option>');
+			}
+			});
+	}
+
+	if(year == "2015"){
+		$.getJSON('./jsons/2015_participate.geojson', function(data){
+			for(var i = 0; i< data.features.length; i++){   
+			   $('#inputcountry').append('<option value="">' + data.features[i].properties.name + '</option>');
+			}
+			});
+		$.getJSON('./jsons/2015_winner.geojson', function(data){
+			for(var i = 0; i< data.features.length; i++){   
+			   $('#inputcountry').append('<option value="">' + data.features[i].properties.name + '</option>');
+			}
+			});
+		$.getJSON('./jsons/2015_stadium.geojson', function(data){
+			for(var i = 0; i< data.features.length; i++){   
+			   $('#inputstadium').append('<option value="">' + data.features[i].properties.name + '</option>');
+			}
+			});
+	}
+	});
+	
 });
--- js/map.js
+++ js/map.js
@@ -1,6 +1,12 @@
 	var mymap = L.map('map').setView([ 3.83203125,
           6.7031073524364783], 4);
 
+function data_17(){
+
+	mymap.eachLayer(function (layer) {
+    mymap.removeLayer(layer);
+	});
+
 	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYW5raXRhMTMyIiwiYSI6ImNqZnBhenl6NTExMXgzM3JxNWhjaGxnczEifQ.GJ09bkYuOB5TAeP0pmsuZg', {
 		maxZoom: 18,
 		id: 'mapbox.streets'
@@ -9,7 +15,7 @@
 	function addDataToMap(data, map){
 		var dataLayer = L.geoJson(data);
 		dataLayer.bindPopup(function(layer){
-			return '<h1 class="head_sta">'+layer.feature.properties.name+'</h1><h2 class="small_sta">'+layer.feature.properties.stadium+'<p class="tiny_sta">'+layer.feature.properties.define+'</p>';
+			return '<h1 class="head_sta">'+layer.feature.properties.name+'</h1><h2 class="small_sta">'+layer.feature.properties.stadium+'<p class="tiny_sta">'+layer.feature.properties.define+'</p><p><button type="button" class="btn btn-warning">View Image</button><button type="button" class="btn btn-info">View Experience</button></p>';
 		}).addTo(map);
 	}
 
@@ -25,7 +31,7 @@ L.geoJson(data, {
 		};
 	}
 }).bindPopup(function(layer){
-	return '<h1 class="head_sta">Participating Nation: <b>'+layer.feature.properties.name+'</b></h1>';
+	return '<h1 class="head_sta">Participating Nation: <b>'+layer.feature.properties.name+'</b></h1><p><button type="button" class="btn btn-warning">View Image</button><button type="button" class="btn btn-info">view Experience</button></p>';
 }).addTo(map);
 
 }
@@ -42,7 +48,8 @@ L.geoJson(data, {
 		};
 	}
 }).bindPopup(function(layer){
-	return '<h1 class="head_sta">Winning Nation: <b>'+layer.feature.properties.name+'</b></h1>';
+
+	return '<h1 class="head_sta">Winning Nation: <b>'+layer.feature.properties.name+'</b></h1><p><button type="button" class="btn btn-warning">View Image</button><button type="button" class="btn btn-info">View Experience</button></p>';
 }).addTo(map);
 
 }
@@ -58,3 +65,81 @@ $.getJSON('./jsons/2017_participate.geojson', function(data){
 $.getJSON('./jsons/2017_winner.geojson', function(data){
 	addWinnerToMap(data, mymap);
 })
+
+}
+
+function data_15(){
+
+	mymap.eachLayer(function (layer) {
+    mymap.removeLayer(layer);
+});
+	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYW5raXRhMTMyIiwiYSI6ImNqZnBhenl6NTExMXgzM3JxNWhjaGxnczEifQ.GJ09bkYuOB5TAeP0pmsuZg', {
+		maxZoom: 18,
+		id: 'mapbox.streets'
+	}).addTo(mymap);
+
+	function addDataToMap(data, map){
+		var dataLayer = L.geoJson(data);
+		dataLayer.bindPopup(function(layer){
+			return '<h1 class="head_sta">'+layer.feature.properties.name+'</h1><h2 class="small_sta">'+layer.feature.properties.stadium+'<p class="tiny_sta">'+layer.feature.properties.define+'</p><p><button type="button" class="btn btn-warning">View Image</button><button type="button" class="btn btn-info">View Experience</button></p>';
+		}).addTo(map);
+	}
+
+function addCountryToMap(data, map){
+L.geoJson(data, {
+	style: function(feature){
+		return{
+			fillColor: "#932aba",
+			fillOpacity: 0.7,
+			stroke: true,
+			color: "grey",
+			weight: 2
+		};
+	}
+}).bindPopup(function(layer){
+	return '<h1 class="head_sta">Participating Nation: <b>'+layer.feature.properties.name+'</b></h1><p><button type="button" class="btn btn-warning">View Image</button><button type="button" class="btn btn-info">view Experience</button></p>';
+}).addTo(map);
+
+}
+
+function addWinnerToMap(data, map){
+L.geoJson(data, {
+	style: function(feature){
+		return{
+			fillColor: "red",
+			fillOpacity: 0.7,
+			stroke: true,
+			color: "grey",
+			weight: 2
+		};
+	}
+}).bindPopup(function(layer){
+	return '<h1 class="head_sta">Winning Nation: <b>'+layer.feature.properties.name+'</b></h1><p><button type="button" class="btn btn-warning">View Image</button><button type="button" class="btn btn-info">View Experience</button></p>';
+}).addTo(map);
+
+}
+
+$.getJSON('./jsons/2015_stadium.geojson', function(data){
+	addDataToMap(data, mymap);
+})
+
+$.getJSON('./jsons/2015_participate.geojson', function(data){
+	addCountryToMap(data, mymap);
+})
+
+$.getJSON('./jsons/2015_winner.geojson', function(data){
+	addWinnerToMap(data, mymap);
+})
+}
+
+$(document).ready(function(){
+	data_17();
+});
+
+$('#2017').click(function(){
+	data_17();
+});
+
+$('#2015').click(function(){
+	data_15();
+});
